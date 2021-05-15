9e04c408b9a38ae07a5ae4178f147e37d32da6d9
Daller9999 <dalerhamzaev@gmail.com>
2021-05-15 13:42:52 +0300

    init commit

---
+++ app/src/main/java/com/sunplacestudio/movieapplication/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.sunplacestudio.movieapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+// Api key - 80dad15a6e9fc96df722cc8eb3da927a
+// eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MGRhZDE1YTZlOWZjOTZkZjcyMmNjOGViM2RhOTI3YSIsInN1YiI6IjYwOWZhMTI5YjIzNGI5MDAyOTQzYjVlNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ZyRn2Jo4nRDqRVJds2gM1FHT2pkHnN6kjNI_Vm67Kl4
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
